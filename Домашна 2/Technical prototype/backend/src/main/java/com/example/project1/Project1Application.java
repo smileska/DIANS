e2782847c4ba40b0d9d46445d87e0c70e1ba2815
eva <115313491+smileska@users.noreply.github.com>
2024-12-03 23:06:29 +0100

    Update Project1Application.java

--- Домашна 2/Technical prototype/backend/src/main/java/com/example/project1/Project1Application.java
+++ Домашна 2/Technical prototype/backend/src/main/java/com/example/project1/Project1Application.java
@@ -3,11 +3,22 @@ package com.example.project1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class Project1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Project1Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Application is running";
     }
 
 }
